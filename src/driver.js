a736e082b527ad5e1a9a79587f9376ea89fb2724
KLDas <19etmm01@uohyd.ac.in>
2022-12-11 21:25:53 +0530

    Added Assignment-1 by KLDas

---
+++ src/driver.js
@@ -0,0 +1,11 @@
+const fileSystems = require("./index.js");
+
+let file = "rubberDuck.txt"
+
+fileSystems.myFileWriter(file, "I am a rubber Duck");
+
+// fileSystems.myFileReader(file);
+
+// fileSystems.myFileUpdater(file, "  and I am Yellow");
+
+// fileSystems.myFileDeleter(file);
---
+++ src/index.js
@@ -0,0 +1,34 @@
+const fs = require("fs/promises");
+
+const myFileWriter = async (fileName, fileContent) => {
+  // write code here
+  // dont chnage function name
+  fs.writeFile(fileName, fileContent, (err) => {
+    console.log(err);
+  });
+};
+
+const myFileReader = async (fileName) => {
+  // write code here
+  // dont chnage function name
+  let data = await fs.readFile(fileName, { encoding: "utf-8" });
+  console.log(data);
+};
+
+const myFileUpdater = async (fileName, fileContent) => {
+  // write code here
+  // dont chnage function name
+  fs.appendFile(fileName, fileContent, (err) => {
+    console.log(err);
+  });
+};
+
+const myFileDeleter = async (fileName) => {
+  // write code here
+  // dont chnage function name
+  fs.unlink(fileName, (err) => {
+    console.log(err);
+  });
+};
+
+module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter };
